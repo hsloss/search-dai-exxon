d1b7aea55d47ecde993e8d4cad3ded2e8422ff38
hsloss <hannahmaguiresloss@gmail.com>
2018-11-16 10:26:00 -0500

    successfully loading variables from JSON file.

--- js/app.js
+++ js/app.js
@@ -10,41 +10,53 @@ let selectedCountry = []
 let typed = input.value.toLocaleLowerCase()
 
 let Variables = function(name, value, definition){
-  this.variableName = name
-  this.variableValue = value
-  this.variableDefinition = definition
+  this.variablesName = name
+  this.variablesValue = value
+  this.variablesDefinition = definition
 }
 
-let MaxOpenLocalContent = new Variables('max_open_local_content', '40', 'max value possible for local content if all awardi… and expat labor from skills percentages/salaires')
-let TotalLocalContent = new Variables('total_local_content', '70', 'prime_local_content + locally_awarded_local_content')
+// let MaxOpenLocalContent = new Variables('max_open_local_content', '40', 'max value possible for local content if all awardi… and expat labor from skills percentages/salaires')
+// let TotalLocalContent = new Variables('total_local_content', '70', 'prime_local_content + locally_awarded_local_content')
 
-variablesArray.push(MaxOpenLocalContent, TotalLocalContent)
+// variablesArray.push(MaxOpenLocalContent, TotalLocalContent)
 
-// $.getJSON('https://raw.githubusercontent.com/hsloss/search-countries/newNamesForVariables/JSON/lcomvars.json', response => {
-//   response.data.forEach((val) => {
-//     let newVar = new Variables(val.name, val.sample_value, val.primary_definition)
-//     variablesArray.push(newVar)
-//   })
-// })
+$.getJSON('https://raw.githubusercontent.com/hsloss/search-countries/newNamesForVariables/JSON/lcomvars.json', response => {
+  response.data.forEach((val) => {
+    let newVar = new Variables(val.name, val.sample_val, val.primary_definition)
+    variablesArray.push(newVar)
+  })
+}).then(function(res){
+  displayFunc()
+  populateForm()
+})
 
-let clickHandler = function(){
+let clickHandler = function(){variablesArray
   removeDefinition()
   for(let i = 0; i < variablesArray.length; i++){
-    if(input.value === variablesArray[i].variableName){
+    if(input.value === variablesArray[i].variablesName){
       selectedCountry.push(variablesArray[i])
-      console.log(variablesArray[i])
       let definition = document.createElement('p')
       paragraph.appendChild(definition)
-      definition.innerText = variablesArray[i].variableDefinition
+      if(variablesArray[i].variablesDefinition === ''){
+        definition.innerText = 'Definition not provided.'
+      } else {
+        definition.innerText = variablesArray[i].variablesDefinition
+      }
     }
   }
   removeSeeAlso()
   for(let i = 0; i < variablesArray.length; i++){
-    let j=0
-    if (variablesArray[i].variableValue === selectedCountry[j].variableValue|| variablesArray[i].countryLanguage === selectedCountry[j].countryLanguage) {
-      let elLI = document.createElement('li')
-      elList.appendChild(elLI)
-      elLI.innerText = variablesArray[i].variableName
+    let variablesNameString = variablesArray[i].variablesName.split('_')
+    let inputValueSplit = input.value.split('_')
+    for(let j = 0; j < inputValueSplit.length; j++) {
+      if (variablesNameString.includes(inputValueSplit[j]) && inputValueSplit[j] !== 'of') {
+        let elLI = document.createElement('li')
+        elList.appendChild(elLI)
+        elLI.innerText = variablesArray[i].variablesName
+        elLI.setAttribute('id', variablesArray[i].variablesName)
+        elLI.addEventListener('click', clickHandlerSeeAlso)
+        break
+      }
     }
   }
   selectedCountry = []
@@ -52,9 +64,8 @@ let clickHandler = function(){
 
 let displayFunc = function(){
   let displayResults = variablesArray.filter(function(test){
-    return test.variableName === typed
+    return test.variablesName === typed
   })
-  console.log(displayResults)
   return displayResults
 }
 
@@ -63,7 +74,7 @@ let populateForm = function() {
   for (let i = 0; i < variablesArray.length; i++) {
     let option = document.createElement('option')
     selectElement.appendChild(option)
-    option.innerText = variablesArray[i].variableName
+    option.innerText = variablesArray[i].variablesName
   }
 }
 
@@ -78,7 +89,9 @@ let removeDefinition = function(){
     paragraph.removeChild(paragraph.firstChild)
   }
 }
+let clickHandlerSeeAlso = function(){
+  input.value = event.target.id
+  clickHandler()
+}
 
 input.addEventListener('change', clickHandler)
-displayFunc()
-populateForm()
